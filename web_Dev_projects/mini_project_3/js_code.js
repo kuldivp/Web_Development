0a87c58f4a2339c894d70ba974141c24a80b2387
kuldeep <kuldeepkkumar2005@gmail.com>
2024-07-04 22:52:29 +0530

    mini_project3

---
+++ web_Dev_projects/mini_project_3/js_code.js
@@ -0,0 +1,18 @@
+const insert=document.getElementById('insert')
+
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML=`<div class='coler'>
+    <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key===" "?"space":e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.Code}</td>
+  </tr>
+</table>
+    </div>`;
+});
